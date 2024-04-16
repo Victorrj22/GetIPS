8df1e31e9683116c02a7d5ef2e0cc01a9df6ea0e
Aluno <victorsoaresrj@hotmail.com>
2024-04-15 21:05:34 -0300

    first commit

---
+++ GetIPS/GetIPS.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+import ipaddress
+import netifaces as ni
+from scapy.all import Ether, ARP, srp, conf
+
+# Obtém o nome da interface de rede
+interface = ni.gateways()['default'][ni.AF_INET][1]
+
+# Obter o endereço IP e a máscara de rede da interface de rede
+endereco_ip = ni.ifaddresses(interface)[ni.AF_INET][0]['addr']
+mascara_rede = ni.ifaddresses(interface)[ni.AF_INET][0]['netmask']
+
+# Encontra todos os IPs no range da máscara de rede
+rede = ipaddress.IPv4Network(endereco_ip + '/' + mascara_rede, strict=False)
+ips_na_rede = [str(ip) for ip in rede.hosts()]
+
+dispositivos_conectados = []
+
+# Faz o "ping" nas redes disponíveis
+for ip in ips_na_rede:
+    resposta = srp(Ether(dst="ff:ff:ff:ff:ff:ff")/ARP(pdst=ip), timeout=2, verbose=0)[0]
+    if resposta:
+        dispositivos_conectados.append({'IP': resposta[0][1].psrc, 'MAC': resposta[0][1].hwsrc})
+
+# Exibe os dispositivos conectados
+for dispositivo in dispositivos_conectados:
+    print("IP:", dispositivo['IP'], "MAC:", dispositivo['MAC'])
